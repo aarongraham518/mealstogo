1446169cf83533ffc6574537d89764cd2a36370d
Aaron <aarongraham518@gmail.com>
2021-06-17 17:19:42 -0400

    details screen for cards added

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,15 +1,15 @@
-import React, {useContext} from 'react';
-import {View} from 'react-native';
-import styled from 'styled-components/native';
-import { ActivityIndicator, Colors } from 'react-native-paper';
+import React, { useContext } from "react";
+import { FlatList, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
-import {Search} from '../../restaurants/components/search.component';
-import {SafeArea} from '../../../components/utility/safe-area.component'; 
-import { FlatList, Pressable } from 'react-native';
-import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
- 
-import {RestaurantsContext} from '../../../services/restaurants/restaurants.context';
+
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
+import { Search } from "../components/search.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
@@ -17,53 +17,44 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
-const Loading = styled(ActivityIndicator)` 
+const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
 `;
-
-const LoadingContainer = styled(View)` 
+const LoadingContainer = styled.View`
   position: absolute;
   top: 50%;
   left: 50%;
 `;
 
-
-export const RestaurantsScreen = ({navigation}) => {
-
-  const {isLoading, restaurants} = useContext(RestaurantsContext);
-  console.log(navigation);
-
+export const RestaurantsScreen = ({ navigation }) => {
+  const { isLoading, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
       {isLoading && (
-        <LoadingContainer style={{position: "absolute", top: "50%", left: "50%"}}>
-          <Loading 
-            size={50}
-            style={{marginLeft: -25}}
-            animating={true} 
-            color={Colors.blue300}
-          />
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
         </LoadingContainer>
       )}
-        
-        <Search />
-        <RestaurantList
-          data={restaurants}
-          renderItem={({item}) => {
-            console.log(item);
-            return (
-            <Pressable onPress={() => navigation.navigate("RestaurantDetail")}>
-                <Spacer position="bottom" size="large">
-                  <RestaurantInfoCard restaurant={item}/>
-                </Spacer>
-            </Pressable>
-            
-          )}}
-          keyExtractor={(item) => item.name}
-        />
-          
-       
+      <Search />
+      <RestaurantList
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("RestaurantDetail", {
+                  restaurant: item,
+                })
+              }
+            >
+              <Spacer position="bottom" size="large">
+                <RestaurantInfoCard restaurant={item} />
+              </Spacer>
+            </TouchableOpacity>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
     </SafeArea>
-  )
+  );
 };
-
