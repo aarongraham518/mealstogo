0e65cffe627e298e8885cd057ef221a6ab07d6b3
Aaron <aarongraham518@gmail.com>
2021-06-23 18:57:29 -0400

    authentation for login and register all set and backed integrated with firebase

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,37 +1,74 @@
-import React, {useState, createContext} from 'react';
-import * as firebase from 'firebase';
+import React, { useState, createContext } from "react";
+import * as firebase from "firebase";
 
-import {loginRequest} from './authentication.service';
+import { loginRequest } from "./authentication.service";
 
 export const AuthenticationContext = createContext();
 
-export const AuthenticationContextProvider = ({children}) => {
-    const [isLoading, setIsLoading] = useState(false);
+export const AuthenticationContextProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
 
-    const [user, setUser] = useState(null);
-    const [error, setError] = useState(null);
+  //check to see if user exists
+  firebase.auth().onAuthStateChanged((usr) => {
+    if(usr){
+      setUser(usr);
+      setIsLoading(false);
+    }else {
+      setIsLoading(false);
+    }
+  });
 
-    const onLogin = (email, password) => {
-        setIsLoading(true);
-        loginRequest(email,password).then((u) => {
-            setUser(u);
-            setIsLoading(false);
-        }).catch((e) => {
-            setIsLoading(false);
-            setError(e);
-        });
-    };
+  const onLogin = (email, password) => {
+    setIsLoading(true);
+    loginRequest(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
 
-    return(
-        <AuthenticationContext.Provider
-            value={{
-                user,
-                isLoading,
-                error,
-                onLogin,
-            }}
-        >
-            {children}
-        </AuthenticationContext.Provider>
-    );
+  const onRegister = (email, password, repeatedPassword) => {
+    setIsLoading(true);
+    if(password !== repeatedPassword){
+      setError("Error: Passwords do not match");
+      return;
+    }
+    firebase
+      .auth().createUserWithEmailAndPassword(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  }
+
+  const onLogout = () => {
+    setUser(null);
+    firebase.auth().signOut();
+  };
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        isAuthenticated: !!user,
+        user,
+        isLoading,
+        error,
+        onLogin,
+        onRegister,
+        onLogout
+      }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
 };
