9fdeab849a93b0f1130fbd6cd23626cd8a89047c
Aaron <aarongraham518@gmail.com>
2021-06-25 14:29:38 -0400

    settings screen updated with favorites and logout screens

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -1,33 +1,30 @@
 import React from "react";
-import { SettingsScreen } from "../../features/settings/components/settings.screen";
+import { SettingsScreen } from "../../features/settings/screens/settings.screen";
+import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
 
 import {
-    createStackNavigator,
-    CardStyleInterpolators,
+  createStackNavigator,
+  CardStyleInterpolators,
 } from "@react-navigation/stack";
 
 const SettingsStack = createStackNavigator();
 
-export const SettingsNavigator = ({route, navigation}) => {
-
-    return (
-        <SettingsStack.Navigator
-            headerMode="screen"
-            screenOptions={{
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+export const SettingsNavigator = ({ route, navigation }) => {
+  return (
+    <SettingsStack.Navigator
+      headerMode="screen"
+      screenOptions={{
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}
+    >
+      <SettingsStack.Screen
+        options={{
+          header: () => null,
         }}
-        >
-        <SettingsStack.Screen
-            options={{
-            header: () => null,
-        }}
-            name="Settings"
-            component={SettingsScreen}
-        />
-        <SettingsStack.Screen 
-            name="Favourites" 
-            component={() => null}
-        />
-        </SettingsStack.Navigator>
-    );
+        name="Settings"
+        component={SettingsScreen}
+      />
+      <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
+    </SettingsStack.Navigator>
+  );
 };
